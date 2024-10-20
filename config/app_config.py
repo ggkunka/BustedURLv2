5029fd97a6b09d7623e2bbd4ca882878d935456a
Jayaprakash N S <60697280+ggkunka@users.noreply.github.com>
2024-10-20 12:34:56 +1100

    Update app_config.py

--- config/app_config.py
+++ config/app_config.py
@@ -3,3 +3,8 @@ KAFKA_BROKER_URL = "localhost:9092"
 REDIS_URL = "redis://localhost:6379/0"
 CHUNK_SIZE = 10000
 IDS_IPS_THRESHOLD = 0.85
+
+# Configuration settings for BustedURL
+
+# Flag to control the usage of XLNet in ensemble learning
+USE_XLNET = True
